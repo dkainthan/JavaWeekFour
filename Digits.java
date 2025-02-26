4342c11831d13109d15e42d9e3a9809de49e9c22
dkainthan <dkainthan@gmail.com>
2025-02-26 10:26:14 -0500

    yrt
    rg

--- Digits.java
+++ Digits.java
@@ -1,4 +1,5 @@
 //************ Diya Kainthan 2/25/2025 Digits.java - prints out different digit`s of a three digit number***** */
+import java.util.*;
 public class Digits {
 public static void main(String[]args) {
     int number;
@@ -6,12 +7,17 @@ public static void main(String[]args) {
     int tens; 
     int hundreds; 
     int thousands;
-    number = 2580;
+    Scanner input = new Scanner(System.in);
+
+    //number = 2580
+    System.out.println("Enter a four digit number: "); 
+    number = input.nextInt();
     ones = number % 10;
     tens = (number / 10) % 10; 
     hundreds =((number / 10) /10) % 10;
     thousands = (((number/10)/10)/10) % 10; 
-    System.out.println("The ones digits is " + ones + " The tens digits is " + tens + " The hundreds digits is " + hundreds + " The thousands place is" + thousands); 
+
+    System.out.println("The ones digits is: " + ones + " The tens digits is: " + tens + " The hundreds digits is: " + hundreds + " The thousands place is: " + thousands); 
 
 }
 }
