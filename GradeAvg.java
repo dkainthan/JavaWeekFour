dea8030dc1ee1b95aa5bb56d9d91884517144a6c
dkainthan <dkainthan@gmail.com>
2025-02-27 10:07:06 -0500

    f
    sadaffsesfdsfdfd

---
+++ GradeAvg.java
@@ -0,0 +1,27 @@
+//Diya Kainthan 2/27/2025 - GradeAverage
+
+import java.util.*;
+public class GradeAvg {
+    public static void main(String[]args){
+        int grade1;
+        int grade2; 
+        int grade3; 
+        int grade4; 
+        int grade5; 
+        double average;
+        Scanner input = new Scanner(System.in); 
+        System.out.println("Enter a grade");
+        grade1 = input.nextInt();
+        System.out.println("Enter a grade");
+        grade2 = input.nextInt();
+        System.out.println("Enter a grade");
+        grade3 = input.nextInt();
+        System.out.println("Enter a grade");
+        grade4 = input.nextInt();
+        System.out.println("Enter a grade");
+        grade5 = input.nextInt();
+        average = ((grade1 + grade2 + grade3 + grade4 + grade5) / 5.0);
+        System.out.println("Your average is " + average); 
+
+    }
+}
