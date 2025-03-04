d84ea1e8c672f4094cc63aebd73aa9d51002e562
dkainthan <dkainthan@gmail.com>
2025-03-04 17:12:01 -0500

    fvvf
    erv

--- Election.java
+++ Election.java
@@ -1,46 +1,50 @@
 import java.util.*;
 public class Election {
     public static void main(String[]args) {
-        int Awbrey_nyVotes; 
-        int Awbrey_njVotes; 
-        int Awbrey_conneticutVotes; 
-        int Martinez_nyVotes; 
-        int Martinez_njVotes;
-        int Martinez_conneticutVotes; 
-        int totalAwbrey; 
-        int totalMartinez; 
+        double Awbrey_nyVotes; 
+        double Awbrey_njVotes; 
+        double Awbrey_conneticutVotes; 
+        double Martinez_nyVotes; 
+        double Martinez_njVotes;
+        double Martinez_conneticutVotes; 
+        double totalAwbrey; 
+        double totalMartinez; 
         double percentageAwbrey; 
         double percentageMartinez; 
-        int totalVotes;
+        double totalVotes;
 
         Scanner input = new Scanner(System.in); 
         System.out.println("Enter Awbrey's New York Votes: "); 
-        Awbrey_nyVotes = input.nextInt();
+        Awbrey_nyVotes = input.nextDouble();
 
         System.out.println("Enter Awbrey's New Jersey Votes: "); 
-        Awbrey_njVotes = input.nextInt();
+        Awbrey_njVotes = input.nextDouble();
 
         System.out.println("Enter Awbrey's Conneticut Votes: "); 
-        Awbrey_conneticutVotes = input.nextInt();
+        Awbrey_conneticutVotes = input.nextDouble();
 
         
         System.out.println("Enter Martinez's New York Votes: "); 
-        Martinez_nyVotes = input.nextInt();
+        Martinez_nyVotes = input.nextDouble();
 
         System.out.println("Enter Awbrey's New Jersey Votes: "); 
-        Martinez_njVotes = input.nextInt();
+        Martinez_njVotes = input.nextDouble();
 
         System.out.println("Enter Awbrey's Conneticut Votes: "); 
-        Martinez_conneticutVotes = input.nextInt();
+        Martinez_conneticutVotes = input.nextDouble();
 
 
         totalAwbrey = Awbrey_nyVotes + Awbrey_njVotes + Awbrey_conneticutVotes; 
         totalMartinez = Martinez_nyVotes + Martinez_njVotes + Martinez_conneticutVotes; 
         totalVotes = Awbrey_nyVotes + Awbrey_njVotes + Awbrey_conneticutVotes + Martinez_nyVotes + Martinez_njVotes + Martinez_conneticutVotes; 
-        percentageAwbrey = totalAwbrey/totalVotes; 
-        percentageMartinez = totalMartinez/totalVotes; 
+        percentageAwbrey = (totalAwbrey/totalVotes) * 100; 
+        percentageMartinez = (totalMartinez/totalVotes) * 100; 
 
-       System.out.println("Awbrey's total votes are " + totalAwbrey + "and the percentage she has is " + percentageAwbrey); 
+       System.out.println("Candidate                    Votes       Percentage"); 
+       System.out.println("Awbrey                    " + totalAwbrey + "     " + percentageAwbrey);  
+       System.out.println("Martinez                  " + totalMartinez + "    " + percentageMartinez); 
+       System.out.println("Total Votes:      " + totalVotes); 
+
 
     }
     
